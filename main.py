44ca4a937b46334eb014275ca28f102bdcc89c04
Lars W <lars@elfenstern.de>
2022-06-19 04:24:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from time import sleep
 import pytubDef
 import logging
-
 if pytubDef.log():
     logging.basicConfig(filename='data/mainDebug.log', encoding='utf-8', level=logging.DEBUG)
     
@@ -10,5 +9,10 @@ print("Starting loop....")
 while True:
     logging.info("Checking for new videos")
     print("Checking for new Videos")
-    pytubDef.loop()
+    try:
+        pytubDef.loop()
+    except Exception as e:
+        logging.info("An error occurred while checking for new videos.")
+        logging.debug(e)
+
     sleep(int(pytubDef.returnInterval()))
